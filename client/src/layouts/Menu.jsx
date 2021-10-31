f29bab13367cca7aca2ca1ec92437b9a682f652e
Evan Henderson <adamhenderson3x3@gmail.com>
2021-10-31 08:21:22 +0700

    add menu background

--- client/src/layouts/Menu.jsx
+++ client/src/layouts/Menu.jsx
@@ -3,14 +3,22 @@ import { useEffect } from "react";
 export default function Menu() {
   useEffect(() => {
     $("input#menu").on("change", function () {
-      if(this.checked === true) $('div.menu__toggler > label > span').text('')
-      else $('div.menu__toggler > label > span').text('-')
+      if (this.checked === true) $("div.menu__toggler > label > span").text("");
+      else $("div.menu__toggler > label > span").text("-");
     });
   });
   return (
     <div className="menu">
       <div className="menu__toggler">
         <input className="invisible" type="checkbox" id="menu" />
+        <div className="menu__background">&nbsp;</div>
+        <div className="menu__content">
+          <ul>
+            <li>
+              <button>Coming soon</button>
+            </li>
+          </ul>
+        </div>
         <label htmlFor="menu">
           <span>-</span>
         </label>
